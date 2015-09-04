f03e9306da741a52ec88784d43f3c70eb49bc606
Josh <jreeseue@gmail.com>
2015-09-04 13:31:10 -0400

    no longer getting hashing error. device not configured.

--- p1/dfs.go
+++ p1/dfs.go
@@ -10,46 +10,13 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
+	// "time"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 
 	"golang.org/x/net/context"
 )
-
-/*
-    Need to implement these types from bazil/fuse/fs!
-
-    type FS interface {
-	  // Root is called to obtain the Node for the file system root.
-	  Root() (Node, error)
-    }
-
-    type Node interface {
-	  // Attr fills attr with the standard metadata for the node.
-	  Attr(ctx context.Context, attr *fuse.Attr) error
-    }
-*/
-
-//=============================================================================
-
-type DFSNode struct {
-	nid   uint64
-	name  string
-	attr  fuse.Attr
-	dirty bool
-	kids  map[string]*DFSNode
-	data  []uint8
-}
-
-var root *DFSNode
-
-type FS struct{}
-
-//  Compile error if DFSNode does not implement interface fs.Node, or if FS does not implement fs.FS
-var _ fs.Node = (*DFSNode)(nil)
-var _ fs.FS = (*FS)(nil)
 
 //=============================================================================
 
@@ -65,22 +32,95 @@ func p_err(s string, args ...interface{}) {
 }
 
 //=============================================================================
+/*
+    Need to implement these types from bazil/fuse/fs!
+
+    type FS interface {
+	  // Root is called to obtain the Node for the file system root.
+	  Root() (Node, error)
+    }
+
+    type Node interface {
+	  // Attr fills attr with the standard metadata for the node.
+	  Attr(ctx context.Context, attr *fuse.Attr) error
+    }
+*/
+
+//=============================================================================
+//  Compile error if DFSNode does not implement interface fs.Node, or if FS does not implement fs.FS
+var _ fs.Node = (*DFSNode)(nil)
+var _ fs.FS = (*FS)(nil)
+
+type DFSNode struct {
+	nid   uint64
+	name  string
+	attr  fuse.Attr
+	dirty bool
+	kids  map[string]*DFSNode
+	data  []uint8
+}
+
+var root *DFSNode
+
+type FS struct{}
 
 // Implement:
-//   func (FS) Root() (n fs.Node, err error)
-//   func (n *DFSNode) Attr(ctx context.Context, attr *fuse.Attr) error
-//   func (n *DFSNode) Lookup(ctx context.Context, name string) (fs.Node, error)
-//   func (n *DFSNode) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error)
-//   func (n *DFSNode) Getattr(ctx context.Context, req *fuse.GetattrRequest, resp *fuse.GetattrResponse) error
-//   func (n *DFSNode) Fsync(ctx context.Context, req *fuse.FsyncRequest) error
-//   func (n *DFSNode) Setattr(ctx context.Context, req *fuse.SetattrRequest, resp *fuse.SetattrResponse) error
-//   func (p *DFSNode) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error)
-//   func (p *DFSNode) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.CreateResponse) (fs.Node, fs.Handle, error)
-//   func (n *DFSNode) ReadAll(ctx context.Context) ([]byte, error)
-//   func (n *DFSNode) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.WriteResponse) error
-//   func (n *DFSNode) Flush(ctx context.Context, req *fuse.FlushRequest) error
-//   func (n *DFSNode) Remove(ctx context.Context, req *fuse.RemoveRequest) error
-//   func (n *DFSNode) Rename(ctx context.Context, req *fuse.RenameRequest, newDir fs.Node) error
+func (FS) Root() (n fs.Node, err error) {
+	fmt.Println("HERE")
+	// n = DFSNode{name: "/", attr: fuse.Attr{Mode: os.ModeDir | 0755}, dirty: false}
+	// n.kids = make(map[string]*DFSNode{mountpoint: root})
+	// n.data = new([]uint8, 64)
+	root = new(DFSNode)
+	root.name = "/"
+	root.kids = make(map[string]*DFSNode)
+	root.data = make([]uint8, 64)
+	return root, nil
+}
+func (DFSNode) Attr(ctx context.Context, attr *fuse.Attr) error {
+	attr.Inode = 1
+	attr.Mode = os.ModeDir | 0555
+	return nil
+}
+
+func (n *DFSNode) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	return n, nil
+}
+
+func (n *DFSNode) ReadDirAll(ctx context.Context) ([]fuse.Dirent, error) {
+	return []fuse.Dirent{}, nil
+}
+func (n *DFSNode) Getattr(ctx context.Context, req *fuse.GetattrRequest, resp *fuse.GetattrResponse) error {
+	return nil
+}
+func (n *DFSNode) Fsync(ctx context.Context, req *fuse.FsyncRequest) error {
+	return nil
+}
+func (n *DFSNode) Setattr(ctx context.Context, req *fuse.SetattrRequest, resp *fuse.SetattrResponse) error {
+	return nil
+}
+func (p *DFSNode) Mkdir(ctx context.Context, req *fuse.MkdirRequest) (fs.Node, error) {
+	return DFSNode{}, nil
+}
+
+func (p *DFSNode) Create(ctx context.Context, req *fuse.CreateRequest, resp *fuse.CreateResponse) (fs.Node, fs.Handle, error) {
+	return DFSNode{}, DFSNode{}, nil
+}
+
+func (n *DFSNode) ReadAll(ctx context.Context) ([]byte, error) {
+	return []byte("HELLO"), nil
+}
+func (n *DFSNode) Write(ctx context.Context, req *fuse.WriteRequest, resp *fuse.WriteResponse) error {
+	return nil
+}
+func (n *DFSNode) Flush(ctx context.Context, req *fuse.FlushRequest) error {
+	return nil
+}
+func (n *DFSNode) Remove(ctx context.Context, req *fuse.RemoveRequest) error {
+	return nil
+}
+func (n *DFSNode) Rename(ctx context.Context, req *fuse.RenameRequest, newDir fs.Node) error {
+	return nil
+}
 
 //=============================================================================
 
@@ -90,35 +130,41 @@ var Usage = func() {
 	flag.PrintDefaults()
 }
 
+var debug bool
+var mountpoint string
+
 func main() {
-	var debug bool
-	var mountpoint string
+	fmt.Println("HELLO")
 
 	flag.Usage = Usage
 	flag.BoolVar(&debug, "debug", true, "debugging")
-	flag.StringVar(&mountpoint, "mount", "dss", "defaults to local 'dss'")
+	flag.StringVar(&mountpoint, "mount", "/tmp/dss", "defaults to local '/tmp/dss'")
 	flag.Parse()
 
 	p_out("main\n")
 
-	root = new(DFSNode)
-	root.init("", os.ModeDir|0755)
-
-	nodeMap[uint64(root.attr.Inode)] = root
-	p_out("root inode %d", int(root.attr.Inode))
+	// nodeMap[uint64(root.attr.Inode)] = root
+	// p_out("root inode %d", int(root.attr.Inode))
+	// p_out("root mode %d", int(root.attr.Mode))
 
 	if _, err := os.Stat(mountpoint); err != nil {
 		os.Mkdir(mountpoint, 0755)
 	}
 	fuse.Unmount(mountpoint)
-	c, err := fuse.Mount(mountpoint, fuse.FSName("dssFS"), fuse.Subtype("project P1"),
-		fuse.LocalVolume(), fuse.VolumeName("dssFS"))
+	c, err := fuse.Mount(
+		mountpoint,
+		fuse.FSName("dssFS"),
+		fuse.Subtype("project P1"),
+		fuse.LocalVolume(),
+		fuse.VolumeName("dssFS"),
+	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer c.Close()
 
 	err = fs.Serve(c, FS{})
+	fmt.Printf("%#v\n", root)
 	if err != nil {
 		log.Fatal(err)
 	}
