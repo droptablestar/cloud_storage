d047fd5f9d09eb536220a81942f6f8a0a6c03769
josh <jreeseue@gmail.com>
2015-11-04 18:40:53 -0500

    trying on ubuntu

--- p4/rpc.go
+++ p4/rpc.go
@@ -33,9 +33,11 @@ func newServerConn(ip string, port int) *ServerConn {
 
 func (s ServerConn) Call(str string, args interface{}, reply interface{}) {
 	for {
+		fmt.Printf("conn: %p addr: %s\n", s.conn, s.addr)
 		for s.conn == nil {
 			s.conn, _ = rpc.Dial("tcp", s.addr)
 		}
+		fmt.Printf("conn: %p addr: %s\n", s.conn, s.addr)
 
 		if err := s.conn.Call(str, args, reply); err == nil {
 			return
@@ -95,41 +97,7 @@ const (
 	PORT = 1234
 )
 
-var newfs string
-var debug = false
-var flusherPeriod = 5
-var modeConsistency = "none"
-var replicaString = "auto"
-
 func main() {
-	var c int
-
-	for {
-		if c = Getopt("cdf:m:r:"); c == EOF {
-			break
-		}
-
-		switch c {
-		case 'c':
-			newfs = "NEWFS "
-		case 'd':
-			debug = !debug
-		case 'f':
-			flusherPeriod, _ = strconv.Atoi(OptArg)
-		case 'm':
-			modeConsistency = OptArg
-		case 'r':
-			replicaString = OptArg
-		default:
-			println("usage: main.go [-c | -d | -f <flush dur> | -m <mode> | -r <rep string>]", c)
-			os.Exit(1)
-		}
-	}
-	fmt.Printf("\nStartup up with debug %v, flush period %v, %smode: %q, replicaStr  %q\n\n",
-		debug, flusherPeriod, newfs, modeConsistency, replicaString)
-
-	loadConfig(replicaString, "config.txt")
-
 	if len(os.Args) > 1 && os.Args[1] == "server" {
 		serveInterface(IP, PORT, new(Arith))
 
